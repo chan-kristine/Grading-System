a74354a7238ad6d97358b8045d3429e0a89111f0
unknown <chankristineagcay@gmail.com>
2021-11-14 14:17:36 +0800

    Setting the value of elif statements in PUP Grading System

---
+++ 1Program-GradingSystem.py
@@ -0,0 +1,45 @@
+import math
+
+Mark = (float(input("Enter you mark:")))
+your_mark= round (Mark)
+if your_mark >= 97 and your_mark <= 100:
+    print("Excellent!")
+    print("Your mark is 1.0!")
+elif your_mark >= 94 and your_mark <= 96:
+    print("Excellent!")
+    print("Your mark is 1.25!")
+elif your_mark >= 91 and  your_mark <= 93:
+    print("Very Good!")
+    print("Your mark is 1.5!")
+elif your_mark >= 88 and your_mark <= 90:
+    print("Very Good!")
+    print("Your mark is 1.75!")
+elif your_mark >= 85 and your_mark <= 87:
+    print("Good!")
+    print("Your mark is 2.0!")
+elif your_mark >= 82 and your_mark <= 84:
+    print("Good!")
+    print("Your mark is 2.25!")
+elif your_mark >= 79 and your_mark <= 81:
+    print("Satisfactory!")
+    print("Your mark is 2.50!")
+elif your_mark >= 76 and your_mark <= 78:
+    print("Satisfactory!")
+    print("Your mark is 2.75!")
+elif your_mark == 75:
+    print("Passing!")
+    print("Your mark is 3.0!")
+elif your_mark >= 65 and your_mark <= 74:
+    print("Failure!")
+    print("Your mark is 5.0!")
+
+else:
+
+    if your_mark == "Inc." :
+        print("Your mark is Incomplete!")
+    elif your_mark == "W" :
+        print("Withdrawn")
+    elif your_mark == "D" :
+        print("Dropped")
+
+print("Your grade is recorded!")
